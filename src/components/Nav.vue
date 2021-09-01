772dad2d354911f2df53ecba7c83b72f6145d07d
Ricardo Martínez <theilluminatiduck@gmail.com>
2021-09-01 00:00:22 -0500

    commiting just because

---
+++ src/components/Nav.vue
@@ -0,0 +1,31 @@
+<template>
+  <nav>
+    <div>
+      <h1><strong>Hey!</strong> Richyhoopd chat app</h1>
+
+      <div v-if="isLogin" class="login">
+        <Avatar :src="user.photoURL" />
+        <button class="text-gray-50 hover:text-black" @click="signOut">
+          Cerrar Sesion
+        </button>
+      </div>
+
+      <button v-else class="bg-green-500 hover:bg-green-600" @click="signIn">
+        Iniciar Sesion
+      </button>
+    </div>
+  </nav>
+</template>
+
+<script>
+import Avatar from './Avatar.vue'
+import { useAuth } from '@/firebase'
+
+export default {
+  components: { Avatar },
+  setup() {
+    const { user, isLogin, signOut, signIn } = useAuth()
+    return { user, isLogin, signOut, signIn }
+  }
+}
+</script>
---
+++ src/firebase.js
@@ -0,0 +1,63 @@
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/firestore'
+
+import Filter from 'bad-words'
+import { ref, onUnmounted, computed } from 'vue'
+
+firebase.initializeApp({
+  apiKey: "AIzaSyA6lqH7Btg20GpO09C9nYjJp5DYZx4sN7A",
+  authDomain: "heychat-1798e.firebaseapp.com",
+  projectId: "heychat-1798e",
+  storageBucket: "heychat-1798e.appspot.com",
+  messagingSenderId: "924733071260",
+  appId: "1:924733071260:web:a709670357c4ba98ffb259",
+  measurementId: "G-2MWX26HSQ5"
+})
+
+const auth = firebase.auth()
+
+export function useAuth() {
+  const user = ref(null)
+  const unsubscribe = auth.onAuthStateChanged(_user => (user.value = _user))
+  onUnmounted(unsubscribe)
+  const isLogin = computed(() => user.value !== null)
+
+  const signIn = async () => {
+    const googleProvider = new firebase.auth.GoogleAuthProvider()
+    await auth.signInWithPopup(googleProvider)
+  }
+  const signOut = () => auth.signOut()
+
+  return { user, isLogin, signIn, signOut }
+}
+
+const firestore = firebase.firestore()
+const messagesCollection = firestore.collection('messages')
+const messagesQuery = messagesCollection.orderBy('createdAt', 'desc').limit(100)
+const filter = new Filter()
+
+export function useChat() {
+  const messages = ref([])
+  const unsubscribe = messagesQuery.onSnapshot(snapshot => {
+    messages.value = snapshot.docs
+      .map(doc => ({ id: doc.id, ...doc.data() }))
+      .reverse()
+  })
+  onUnmounted(unsubscribe)
+
+  const { user, isLogin } = useAuth()
+  const sendMessage = text => {
+    if (!isLogin.value) return
+    const { photoURL, uid, displayName } = user.value
+    messagesCollection.add({
+      userName: displayName,
+      userId: uid,
+      userPhotoURL: photoURL,
+      text: filter.clean(text),
+      createdAt: firebase.firestore.FieldValue.serverTimestamp()
+    })
+  }
+
+  return { messages, sendMessage }
+}
